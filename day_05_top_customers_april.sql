67c027785c08b5f3999f2ee70505d76e2aad17b8
amansqlprojects <amanguptacons@gmail.com>
2025-07-02 10:13:12 +0530

    day_05_top_customers_april.sql

---
+++ day_05_top_customers_april.sql
@@ -0,0 +1,16 @@
+  -- Day 5 Challenge: Top 3 Customers by Sales in April
+
+SELECT
+  c.name AS customer_name,
+  SUM(o.total_amount) AS april_sales
+FROM
+  orders o
+JOIN
+  customers c ON o.customer_id = c.customer_id
+WHERE
+  DATE_PART('month', o.order_date) = 4
+GROUP BY
+  c.name
+ORDER BY
+  april_sales DESC
+LIMIT 3;
