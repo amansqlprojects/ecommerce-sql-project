7e409e86cde6b0daacb94ebaf8da24ebcb37ce57
amansqlprojects <amanguptacons@gmail.com>
2025-07-15 16:41:56 +0530

    day_17_spending_increase.sql

--- day_17_spending_increase.sql
+++ day_17_spending_increase.sql
@@ -1,19 +1,19 @@
 -- Day 17: List customer orders where the current order is higher than the previous one
 
-SELECT
-  c.customer_id,
-  c.name,
-  o.order_id,
-  o.order_date,
-  o.total_amount,
-  LAG(o.total_amount) OVER (
-    PARTITION BY c.customer_id ORDER BY o.order_date
-  ) AS previous_order_amount
-FROM
-  customers c
-JOIN orders o ON c.customer_id = o.customer_id
-QUALIFY
-  o.total_amount > LAG(o.total_amount) OVER (
-    PARTITION BY c.customer_id ORDER BY o.order_date
-  )
-ORDER BY c.customer_id, o.order_date;
+SELECT *
+FROM (
+  SELECT
+    c.customer_id,
+    c.name,
+    o.order_id,
+    o.order_date,
+    o.total_amount,
+    LAG(o.total_amount) OVER (
+      PARTITION BY c.customer_id ORDER BY o.order_date
+    ) AS previous_order_amount
+  FROM
+    customers c
+  JOIN orders o ON c.customer_id = o.customer_id
+) AS sub
+WHERE sub.total_amount > sub.previous_order_amount
+ORDER BY sub.customer_id, sub.order_date;
