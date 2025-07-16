dcf08c17f16bf3ed2f68d8a25c521ce9131a8c4a
amansqlprojects <amanguptacons@gmail.com>
2025-07-16 18:10:24 +0530

    day18_running_total.sql

---
+++ day18_running_total.sql
@@ -0,0 +1,13 @@
+-- Day 18: For each order, show the order amount and the running total of orders
+
+SELECT
+  o.order_id,
+  o.order_date,
+  o.total_amount,
+  SUM(o.total_amount) OVER (
+    ORDER BY o.order_date
+    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+  ) AS running_total
+FROM
+  orders o
+ORDER BY o.order_date;
