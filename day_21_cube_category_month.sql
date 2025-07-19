41d021364608ea3ebf721717a1929521513a7418
amansqlprojects <amanguptacons@gmail.com>
2025-07-19 19:14:01 +0530

    day_21_cube_category_month.sql

---
+++ day_21_cube_category_month.sql
@@ -0,0 +1,11 @@
+--Day 21 Challenge: how sales for each product category per month, including all subtotals across dimensions
+
+SELECT
+  p.category,
+  DATE_TRUNC('month', o.order_date) AS order_month,
+  SUM(oi.quantity * oi.item_price) AS total_sales
+FROM orders o
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY CUBE (p.category, DATE_TRUNC('month', o.order_date))
+ORDER BY p.category NULLS LAST, order_month NULLS LAST;
