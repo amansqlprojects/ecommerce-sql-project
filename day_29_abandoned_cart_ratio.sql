9518bfd02044769edd55fb79c6113cf2a8ce87dd
amansqlprojects <amanguptacons@gmail.com>
2025-08-04 09:43:22 +0530

    day_29_abandoned_cart_ratio.sql

---
+++ day_29_abandoned_cart_ratio.sql
@@ -0,0 +1,10 @@
+--Day 29 Challenge: Number of customers who browsed but never placed an order
+
+WITH browsing_only AS (
+  SELECT DISTINCT ca.customer_id
+  FROM customer_activity ca
+  WHERE activity_type = 'browsed'
+    AND ca.customer_id NOT IN (SELECT DISTINCT customer_id FROM orders)
+)
+SELECT COUNT(*) AS abandoned_customers
+FROM browsing_only;
