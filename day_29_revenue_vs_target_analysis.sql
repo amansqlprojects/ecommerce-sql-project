bd03d3f2986305b5c65b0f3a1acca3163a8a4b34
amansqlprojects <amanguptacons@gmail.com>
2025-08-04 09:40:30 +0530

    day_29_revenue_vs_target_analysis.sql

---
+++ day_29_revenue_vs_target_analysis.sql
@@ -0,0 +1,20 @@
+--Day 29 Challenge: Create Monthly Revenue versus Target
+
+WITH monthly_revenue AS (
+  SELECT 
+    DATE_TRUNC('month', order_date) AS month,
+    SUM(total_amount) AS actual_revenue
+  FROM orders
+  GROUP BY month
+),
+comparison AS (
+  SELECT 
+    t.month,
+    t.target_amount,
+    r.actual_revenue,
+    (r.actual_revenue - t.target_amount) AS variance
+  FROM monthly_targets t
+  LEFT JOIN monthly_revenue r ON t.month = r.month
+)
+SELECT * FROM comparison
+ORDER BY month;
